e5a664b9d0a068c6579b21c49621f203c04aadbd
Taabeer Shah <107323025+Taabeer@users.noreply.github.com>
2024-10-20 17:48:32 -0400

    Create recipes_schema.sql

---
+++ recipes_schema.sql
@@ -0,0 +1,83 @@
+CREATE TABLE Users 
+(userName VARCHAR(20) NOT NULL, 
+ password VARCHAR(20) NOT NULL, 
+ PRIMARY KEY (userName)); 
+
+
+CREATE TABLE Recipes 
+(recipeId VARCHAR(40) NOT NULL, 
+ title VARCHAR(100) NOT NULL, 
+ yield INTEGER DEFAULT 1 NOT NULL CHECK (yield > 0), 
+ text TEXT NOT NULL, 
+ calories INTEGER CHECK (calories >= 0), 
+ PRIMARY KEY(recipeId),
+ UNIQUE(title)); 
+
+
+CREATE TABLE Ingredients 
+(foodId VARCHAR(35) NOT NULL, 
+ text TEXT NOT NULL, 
+ quantity INTEGER NOT NULL CHECK (quantity > 0), 
+ measure TEXT, 
+ weight INTEGER CHECK (weight IS NULL OR weight >= 0), 
+ PRIMARY KEY (foodId));
+
+
+CREATE TABLE Cuisines 
+(cuisineName VARCHAR(20) NOT NULL, 
+ text TEXT, 
+ PRIMARY KEY (cuisineName)); 
+
+
+CREATE TABLE Labels 
+(label TEXT NOT NULL CHECK (LENGTH(label) > 0),  
+ PRIMARY KEY (label));
+
+
+CREATE TABLE Reviews 
+(reviewId INTEGER NOT NULL CHECK (reviewId > 0), 
+ userName VARCHAR(20) NOT NULL, 
+ recipeId VARCHAR(40) NOT NULL, 
+ title VARCHAR(50), 
+ text TEXT, 
+ timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP CHECK (timestamp <= CURRENT_TIMESTAMP), 
+ PRIMARY KEY (reviewId, userName), 
+ FOREIGN KEY (recipeId) REFERENCES Recipes, 
+ FOREIGN KEY (userName) REFERENCES Users);
+
+
+CREATE TABLE Owns 
+(userName VARCHAR(20),
+ recipeId VARCHAR(40), 
+ PRIMARY KEY (userName, recipeId), 
+ FOREIGN KEY (userName) REFERENCES Users, 
+ FOREIGN KEY (recipeId) REFERENCES Recipes ON DELETE CASCADE); 
+
+
+CREATE TABLE Likes 
+(userName VARCHAR(20), 
+ recipeId VARCHAR(40), 
+ PRIMARY KEY (userName, recipeId), 
+ FOREIGN KEY (userName) REFERENCES Users, 
+ FOREIGN KEY (recipeId) REFERENCES Recipes ON DELETE CASCADE);
+
+CREATE TABLE Contains_ingredients 
+(recipeId VARCHAR(40), 
+ foodId VARCHAR(35), 
+ PRIMARY KEY (recipeId, foodId), 
+ FOREIGN KEY (recipeId) REFERENCES Recipes ON DELETE CASCADE, 
+ FOREIGN KEY (foodId) REFERENCES Ingredients);
+
+CREATE TABLE Contains_labels 
+(recipeId VARCHAR(40), 
+ label TEXT, 
+ PRIMARY KEY (recipeId, label), 
+ FOREIGN KEY (recipeId) REFERENCES Recipes ON DELETE CASCADE, 
+ FOREIGN KEY (label) REFERENCES Labels); 
+
+CREATE TABLE Contains_cuisines 
+(recipeId VARCHAR(40), 
+ cuisineName VARCHAR(20), 
+ PRIMARY KEY (recipeId, cuisineName), 
+ FOREIGN KEY (recipeId) REFERENCES Recipes ON DELETE CASCADE, 
+ FOREIGN KEY (cuisineName) REFERENCES Cuisines);
