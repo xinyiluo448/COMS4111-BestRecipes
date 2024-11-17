dbb924d8ba587fa4755a5fe67237f9dfe4ece0c9
Xinyi Luo <xinyiluo2957@gmail.com>
2024-11-17 08:07:43 +0000

    Add base template and handle user session display

--- webserver/server.py
+++ webserver/server.py
@@ -15,59 +15,59 @@ conn = engine.connect()
 
 metadata=MetaData()
 users = Table('users', metadata,
-    Column('username', String(20), primary_key=True, nullable=False),
-    Column('password', String(20), nullable=False)
+	Column('username', String(20), primary_key=True, nullable=False),
+	Column('password', String(20), nullable=False)
 )
 recipes = Table('recipes', metadata,
-    Column('recipeid', String(40), primary_key=True, nullable=False),
-    Column('title', String(100), nullable=False, unique=True),
-    Column('yield', Float, nullable=False, default=1.0),
-    Column('text', String, nullable=False),
-    Column('calories', Integer, nullable=True),
-    CheckConstraint('yield> 0', name='yield_check'),
-    CheckConstraint('calories >= 0', name='calories_check')
+	Column('recipeid', String(40), primary_key=True, nullable=False),
+	Column('title', String(100), nullable=False, unique=True),
+	Column('yield', Float, nullable=False, default=1.0),
+	Column('text', String, nullable=False),
+	Column('calories', Integer, nullable=True),
+	CheckConstraint('yield> 0', name='yield_check'),
+	CheckConstraint('calories >= 0', name='calories_check')
 )
 ingredients = Table('ingredients', metadata,
-    Column('foodid', String(35), primary_key=True, nullable=False),
-    Column('food', String(100), nullable=False)
+	Column('foodid', String(35), primary_key=True, nullable=False),
+	Column('food', String(100), nullable=False)
 )
 cuisines= Table('cuisines',metadata,
-    Column('cuisinename',String(20),primary_key=True,nullable=False),
-    Column('text',String,nullable=True)
+	Column('cuisinename',String(20),primary_key=True,nullable=False),
+	Column('text',String,nullable=True)
 )
 labels= Table('labels',metadata,
-    Column('labelname',String(20),primary_key=True,nullable=False),
-    Column('text',String,nullable=True)
+	Column('labelname',String(20),primary_key=True,nullable=False),
+	Column('text',String,nullable=True)
 )
 reviews= Table('reviews',metadata,
-    Column('reviewid',Integer,primary_key=True,nullable=False),
-    Column('username',String(20),ForeignKey('users.username'),nullable=False),
-    Column('recipeid', String(40),ForeignKey('recipes.recipeid'),nullable=False),
-    Column('title',String(50)),
-    Column('text',Text),
-    Column('timestamp',TIMESTAMP,nullable=False,default="CURRENT_TIMESTAMP"),
-    CheckConstraint('timestamp<= CURRENT_TIMESTAMP',name='timestamp_check')
+	Column('reviewid',Integer,primary_key=True,nullable=False),
+	Column('username',String(20),ForeignKey('users.username'),nullable=False),
+	Column('recipeid', String(40),ForeignKey('recipes.recipeid'),nullable=False),
+	Column('title',String(50)),
+	Column('text',Text),
+	Column('timestamp',TIMESTAMP,nullable=False,default="CURRENT_TIMESTAMP"),
+	CheckConstraint('timestamp<= CURRENT_TIMESTAMP',name='timestamp_check')
 )
 
 owns= Table('owns', metadata,
-    Column('username', String(20), ForeignKey('users.username'), primary_key=True, nullable=False),
-    Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False)
+	Column('username', String(20), ForeignKey('users.username'), primary_key=True, nullable=False),
+	Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False)
 )
 likes= Table('likes', metadata,
-    Column('username', String(20), ForeignKey('users.username'), primary_key=True, nullable=False),
-    Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False)
+	Column('username', String(20), ForeignKey('users.username'), primary_key=True, nullable=False),
+	Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False)
 )
 contains_ingredients = Table('contains_ingredients', metadata,
-    Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False),
-    Column('foodid', String(35), ForeignKey('ingredients.foodid'), primary_key=True, nullable=False)
+	Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False),
+	Column('foodid', String(35), ForeignKey('ingredients.foodid'), primary_key=True, nullable=False)
 )
 contains_labels = Table('contains_labels', metadata,
-    Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False),
-    Column('labelname', String(20), ForeignKey('labels.labelname'), primary_key=True, nullable=False)
+	Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False),
+	Column('labelname', String(20), ForeignKey('labels.labelname'), primary_key=True, nullable=False)
 )
 contains_cuisines = Table('contains_cuisines', metadata,
-    Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False),
-    Column('cuisinename', String(20), ForeignKey('cuisines.cuisinename'), primary_key=True, nullable=False)
+	Column('recipeid', String(40), ForeignKey('recipes.recipeid', ondelete='CASCADE'), primary_key=True, nullable=False),
+	Column('cuisinename', String(20), ForeignKey('cuisines.cuisinename'), primary_key=True, nullable=False)
 )
 
 @app.before_request
@@ -131,9 +131,10 @@ def profile(username = None):
 			print("User not logged in, directing to login page")
 			return redirect(url_for('login'))
 	else:
+		is_logged_in = True if 'username' in session else False
 		user = g.conn.execute(text('SELECT * FROM Users WHERE username = (:name)'), {"name":username}).fetchone()
 		if not user:
-			return render_template('profile.html', user=None)
+			return render_template('profile.html', user=None, is_logged_in=is_logged_in)
 
 		query_reviews = text("""
 			SELECT r.reviewId, r.recipeId, r.title, r.text, r.timestamp 
@@ -158,7 +159,12 @@ def profile(username = None):
 		""")
 		liked_recipes = g.conn.execute(query_recipes, {"name": username}).fetchall()
 
-		return render_template('profile.html', user=user, reviews=reviews, recipes=recipes, liked_recipes=liked_recipes)
+		return render_template('profile.html', 
+								user=user, 
+								reviews=reviews, 
+								recipes=recipes, 
+								liked_recipes=liked_recipes,
+								is_logged_in=is_logged_in)
 
 @app.route('/recipe/<recipe_id>')
 def recipe(recipe_id):
@@ -228,11 +234,172 @@ def like_recipe(recipe_id):
 @app.route('/')
 @app.route('/recipes')
 def show_recipes():
-    cursor= g.conn.execute(text("SELECT * FROM recipes"))
-    recipes= []
-    for row in cursor:
-        recipes.append(row)
-    return render_template('recipes.html', recipes=recipes)
+	print("show recipes")
+	is_logged_in = True if 'username' in session else False
+	cursor= g.conn.execute(text("SELECT * FROM recipes"))
+	recipes= []
+	for row in cursor:
+		recipes.append(row)
+	return render_template('recipes.html', recipes=recipes, is_logged_in=is_logged_in)
+
+@app.route('/insert-recipe')
+def insert_recipe():
+	is_logged_in = True if 'username' in session else False
+	label_result = g.conn.execute(text('SELECT labelname FROM labels'))
+	labels=[]
+	for row in label_result:
+		labels.append(row)
+	cuisines_result = g.conn.execute(text('SELECT cuisinename FROM cuisines'))
+	cuisines=[]
+	for row in cuisines_result:
+		cuisines.append(row)
+
+	return render_template('insertrecipe.html', 
+							labels=labels, 
+							cuisines=cuisines, 
+							is_logged_in=is_logged_in)
+
+@app.route('/submit-recipe', methods=['POST'])
+# Fix: should NOT go ahead with insertion of recipe if there is an issue with ingredient, cuisine, or label insertion
+# Fix: to include text from label + cuisine user insertion 
+# Fix: add user as parameter, redirect to user page after they press submit
+def submit_recipe():
+	try:
+		title=request.form['recipe-title']
+		yield_value=request.form['yield']
+		calories=request.form['calories']
+		description=request.form['description']
+		recipe_id = str(uuid.uuid4())
+		recipe_insert = insert(recipes).values(
+			recipeid=recipe_id,
+			title=title,
+			**{'yield': yield_value},
+			text=description,
+			calories=calories
+		)
+		g.conn.execute(recipe_insert)
+		g.conn.commit()
+		ingredients_list = request.form.getlist('ingredients[]')
+		def generate_custom_uuid(length=35):
+			uuid_str=str(uuid.uuid4()).replace('-', '')
+			return uuid_str[:length]
+		for ingredient in ingredients_list:
+			ingredient_id = generate_custom_uuid()
+			existing_ingredient=select(ingredients.c.food).where(ingredients.c.food == ingredient)
+			cursor = g.conn.execute(existing_ingredient).fetchone()
+			if not cursor:
+				new_ingredient=insert(ingredients).values(
+					food=ingredient,
+					foodid=ingredient_id
+				)
+				g.conn.execute(new_ingredient)
+		g.conn.commit()
+		for ingredient in ingredients_list:
+			curr_ingredient=select(ingredients.c.foodid).where(ingredients.c.food == ingredient)
+			cursor = g.conn.execute(curr_ingredient).fetchone()
+			if cursor:
+				food_id= cursor[0] 
+				new_insert= insert(contains_ingredients).values(
+				recipeid= recipe_id,
+				foodid=food_id
+			)
+			g.conn.execute(new_insert)
+			g.conn.commit() 
+		labels_list=request.form.getlist('labels[]')
+		new_label=request.form['new-label']
+		if new_label:
+			labels_list.append(new_label)
+		for label in labels_list:
+			existing_label= select(labels.c.labelname).where(labels.c.labelname == label)
+			cursor= g.conn.execute(existing_label).fetchone()
+			if not cursor:
+				new_label_insert= insert(labels).values(labelname=label, text=None)
+				g.conn.execute(new_label_insert)
+				g.conn.commit()
+			label_insert = insert(contains_labels).values(recipeid=recipe_id, labelname=label)
+			g.conn.execute(label_insert)
+			g.conn.commit()
+		cuisines_list=request.form.getlist('cuisines[]')
+		new_cuisine=request.form['new-cuisine']
+		if new_cuisine:
+			cuisines_list.append(new_cuisine)
+		for cuisine in cuisines_list:
+			existing_cuisine= select(cuisines.c.cuisinename).where(cuisines.c.cuisinename == cuisine)
+			cursor= g.conn.execute(existing_cuisine).fetchone()
+			if not cursor:
+				fresh_insert= insert(cuisines).values(cuisinename=cuisine)
+				g.conn.execute(fresh_insert)
+				g.conn.commit()
+			cuisines_insert= insert(contains_cuisines).values(recipeid=recipe_id, cuisinename=cuisine)
+			g.conn.execute(cuisines_insert)
+			g.conn.commit()
+
+		return redirect('/recipes')  
+	except Exception as e:
+		return f"Error: {e}"
+
+# edit recipe BUT only if you are the owner-> redirects to something like insert recipe page (add edit button on frontend)
+# delete recipe but if you the owner (add delete button on frontend)
+# write a review + display existing reviews
+# search functionality:
+@app.route('/search')
+def search_page():
+	is_logged_in = 'username' in session
+	statement = select(cuisines.c.cuisinename)
+	statement2 = select(labels.c.labelname)
+	cursor = g.conn.execute(statement)
+	cuisines_list = [{'cuisinename': row.cuisinename} for row in cursor]
+	cursor = g.conn.execute(statement2)
+	labels_list = [{'labelname': row.labelname} for row in cursor]
+	return render_template('recipesearch.html',
+							cuisines=cuisines_list,
+							labels=labels_list,
+							is_logged_in=is_logged_in)
+
+@app.route('/search-recipe', methods=['GET'])
+def search_recipe():
+	is_logged_in = 'username' in session
+
+	search_term= request.args.get('searchTerm', '').strip()
+	selected_cuisines= request.args.getlist('cuisines')
+	selected_labels= request.args.getlist('labels')
+	selected_ingredients = request.args.getlist('ingredients')
+	min_likes = request.args.get('min_likes', type=int)
+	cuisines_found= []
+	recipes_found= []
+	labels_found=[]
+	ingredients_found = []
+	statement= select(recipes.c.recipeid, recipes.c.title, recipes.c['yield'], recipes.c.calories, recipes.c.text)\
+		.where(recipes.c.title.ilike(f"%{search_term}%"))
+	if selected_cuisines:
+		statement= statement.join(contains_cuisines, contains_cuisines.c.recipeid == recipes.c.recipeid)\
+			.where(contains_cuisines.c.cuisinename.in_(selected_cuisines))
+	if selected_labels:
+		statement= statement.join(contains_labels, contains_labels.c.recipeid == recipes.c.recipeid)\
+			.where(contains_labels.c.labelname.in_(selected_labels))
+	if min_likes:
+		statement = statement.join(
+			likes, likes.c.recipeid == recipes.c.recipeid
+		).group_by(recipes.c.recipeid).having(func.count(likes.c.recipeid) >= min_likes)
+	if selected_ingredients:
+		statement = statement.join(contains_ingredients, contains_ingredients.c.recipeid == recipes.c.recipeid)\
+			.join(ingredients, ingredients.c.foodid == contains_ingredients.c.foodid)\
+			.where(ingredients.c.food.in_(selected_ingredients))
+	cursor = g.conn.execute(statement)
+	recipes_found= cursor.fetchall()
+	cuisines_query= select(cuisines.c.cuisinename)
+	labels_query= select(labels.c.labelname)
+	ingredients_query= select(ingredients.c.food)
+	cuisines_found= g.conn.execute(cuisines_query).fetchall()
+	labels_found= g.conn.execute(labels_query).fetchall()
+	ingredients_found= g.conn.execute(ingredients_query).fetchall()
+
+	return render_template('recipes.html', 
+							recipes=recipes_found,
+							cuisines=cuisines_found,
+							labels=labels_found,
+							ingredients=ingredients_found,
+							is_logged_in=is_logged_in)
 
 if __name__ == "__main__":
 	import click
@@ -241,7 +408,7 @@ if __name__ == "__main__":
 	@click.option('--debug', is_flag=True)
 	@click.option('--threaded', is_flag=True)
 	@click.argument('HOST', default='0.0.0.0')
-	@click.argument('PORT', default=8996, type=int)
+	@click.argument('PORT', default=8889, type=int)
 	def run(debug, threaded, host, port):
 		HOST, PORT = host, port
 		print("running on %s:%d" % (HOST, PORT))
