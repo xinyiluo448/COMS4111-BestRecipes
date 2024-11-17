4d8530e6c82ac87b79d6626ad09ee5f4d5fcefe4
Xinyi Luo <xinyiluo2957@gmail.com>
2024-11-17 00:23:53 +0000

    Add login, register, profile html

--- webserver/server.py
+++ webserver/server.py
@@ -3,7 +3,7 @@
 Columbia's COMS W4111.001 Introduction to Databases
 Example Webserver
 To run locally:
-    python3 server.py
+	python3 server.py
 Go to http://localhost:8111 in your browser.
 A debugger such as "pdb" may be helpful for debugging.
 Read about it online.
@@ -12,24 +12,12 @@ import os
   # accessible as a variable in index.html:
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response, abort
+from flask import Flask, request, render_template, g, redirect, Response, abort, url_for
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
-# XXX: The URI should be in the format of:
-#
-#     postgresql://user:password@104.196.222.236/proj1part2
-#
-# For example, if you had username gravano and password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://gravano:foobar@104.196.222.236/proj1part2"
-#
-DATABASEURI = "postgresql://user:password@104.196.222.236/proj1part2"
+DATABASEURI = "postgresql://ts3585:751429@104.196.222.236/proj1part2"
 
 
 #
@@ -45,129 +33,119 @@ conn = engine.connect()
 
 # The string needs to be wrapped around text()
 
-conn.execute(text("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);"""))
-conn.execute(text("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');"""))
+# conn.execute(text("""CREATE TABLE IF NOT EXISTS test (
+#   id serial,
+#   name text
+# );"""))
+# conn.execute(text("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');"""))
 
 # To make the queries run, we need to add this commit line
 
-conn.commit() 
+# conn.commit() 
 
 @app.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request.
+	print("before")
+	"""
+	This function is run at the beginning of every web request
+	(every time you enter an address in the web browser).
+	We use it to setup a database connection that can be used throughout the request.
 
-  The variable g is globally accessible.
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print("uh oh, problem connecting to database")
-    import traceback; traceback.print_exc()
-    g.conn = None
+	The variable g is globally accessible.
+	"""
+	try:
+		g.conn = engine.connect()
+	except:
+		print("uh oh, problem connecting to database")
+		import traceback; traceback.print_exc()
+		g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't, the database could run out of memory!
-  """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
+	print("after")
+	"""
+	At the end of the web request, this makes sure to close the database connection.
+	If you don't, the database could run out of memory!
+	"""
+	try:
+		g.conn.close()
+		print("conn closed")
+	except Exception as e:
+		print("Exception: {e}")
+		pass
 
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-#
-# see for routing: https://flask.palletsprojects.com/en/2.0.x/quickstart/?highlight=routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
+	"""
+	request is a special object that Flask provides to access web request information:
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+	request.method:   "GET" or "POST"
+	request.form:     if the browser submitted a form, this contains the data in the form
+	request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
 
-  See its API: https://flask.palletsprojects.com/en/2.0.x/api/?highlight=incoming%20request%20data
+	See its API: https://flask.palletsprojects.com/en/2.0.x/api/?highlight=incoming%20request%20data
 
-  """
+	"""
 
-  # DEBUG: this is debugging code to see what request looks like
-  print(request.args)
-
-
-  #
-  # example of a database query 
-  #
-  cursor = g.conn.execute(text("SELECT name FROM test"))
-  g.conn.commit()
-
-  # 2 ways to get results
-
-  # Method 1 - Indexing result by column number
-  names = []
-  for result in cursor:
-    names.append(result[0])  
-
-  # Method 2 - Indexing result by column name
-  # names = []
-  # results = cursor.mappings().all()
-  # for result in results:
-  #   names.append(result["name"])
-
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #
-  #     # creates a <div> tag for each element in data
-  #     # will print:
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+	# DEBUG: this is debugging code to see what request looks like
+	print(request.args)
 
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
+	#
+	# example of a database query 
+	#
+	# cursor = g.conn.execute(text("SELECT name FROM test"))
+	# g.conn.commit()
+
+	# 2 ways to get results
+
+	# Method 1 - Indexing result by column number
+	names = []
+	# for result in cursor:
+	#   names.append(result[0])  
+
+	# Method 2 - Indexing result by column name
+	# names = []
+	# results = cursor.mappings().all()
+	# for result in results:
+	#   names.append(result["name"])
+
+	# cursor.close()
+
+	#
+	# Flask uses Jinja templates, which is an extension to HTML where you can
+	# pass data to a template and dynamically generate HTML based on the data
+	# (you can think of it as simple PHP)
+	# documentation: https://realpython.com/primer-on-jinja-templating/
+	#
+	# You can see an example template in templates/index.html
+	#
+	# context are the variables that are passed to the template.
+	# for example, "data" key in the context variable defined below will be
+	# accessible as a variable in index.html:
+	#
+	#     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+	#     <div>{{data}}</div>
+	#
+	#     # creates a <div> tag for each element in data
+	#     # will print:
+	#     #
+	#     #   <div>grace hopper</div>
+	#     #   <div>alan turing</div>
+	#     #   <div>ada lovelace</div>
+	#     #
+	#     {% for n in data %}
+	#     <div>{{n}}</div>
+	#     {% endfor %}
+	#
+	context = dict(data = names)
+	print("index")
+
+	#
+	# render_template looks in the templates/ folder for files.
+	# for example, the below file reads template/index.html
+	#
+	return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at:
@@ -179,48 +157,89 @@ def index():
 #
 @app.route('/another')
 def another():
-  return render_template("another.html")
+	print("another")
+	return render_template("another.html")
 
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add(): 
-  name = request.form['name']
-  params_dict = {"name":name}
-  g.conn.execute(text('INSERT INTO test(name) VALUES (:name)'), params_dict)
-  g.conn.commit()
-  return redirect('/')
+	name = request.form['name']
+	params_dict = {"name":name}
+	g.conn.execute(text('INSERT INTO test(name) VALUES (:name)'), params_dict)
+	g.conn.commit()
+	return redirect('/')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'POST':
+		username = request.form['username']
+		try:
+			print("inserting into db")
+			g.conn.execute(text('INSERT INTO Users (username) VALUES (?)'), (username,))
+			g.conn.commit()
+			session['username'] = username
+			print("done")
+			return redirect('/')
+		except IntegrityError as e:
+			print("error: {e}")
+			return render_template('register.html', error="Username already exists: {e}.")
+
+		except Exception as e:
+			print("error: {e}")
+			return render_template('register.html', error="An error occurred: {e}.")
+
+	return render_template('register.html')
 
 @app.route('/login')
 def login():
-    abort(401)
-    this_is_never_executed()
+	print("login")
+	if request.method == 'POST':
+		username = request.form['username']
+		user = g.conn.execute(text('SELECT * FROM Users WHERE username = ?'), (username,)).fetchone()
+		if user:
+			print(f"User {username} found!")
+			session['username'] = user['username']
+			return redirect('/')
+	else:
+		return "User not found!"  # You can return a custom error page
+	return render_template("login.html")
+
+@app.route('/profile/<username>')
+def profile(username):
+	# Check if the user is logged in by checking the session
+	username = session.get('username', None)
+	
+	if username is None:
+		return redirect(url_for('login'))
+	else:
+		user = g.conn.execute('SELECT * FROM Users WHERE username = ?', (username,)).fetchone()
+		return render_template('profile.html', user=user)
 
 
 if __name__ == "__main__":
-  import click
+	import click
 
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using:
+	@click.command()
+	@click.option('--debug', is_flag=True)
+	@click.option('--threaded', is_flag=True)
+	@click.argument('HOST', default='0.0.0.0')
+	@click.argument('PORT', default=8000, type=int)
+	def run(debug, threaded, host, port):
+		"""
+		This function handles command line parameters.
+		Run the server using:
 
-        python3 server.py
+			python3 server.py
 
-    Show the help text using:
+		Show the help text using:
 
-        python3 server.py --help
+			python3 server.py --help
 
-    """
+		"""
 
-    HOST, PORT = host, port
-    print("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+		HOST, PORT = host, port
+		print("running on %s:%d" % (HOST, PORT))
+		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
-  run()
+	run()
